a70ddf31323de3ab4ee58c4ba4d396747f59329c
i.yusubov555@gmail.com <i.yusubov555@gmail.com>
2019-06-20 12:05:35 +0400

    First Commit

---
+++ com.scmloader.test3/Main.java
@@ -0,0 +1,13 @@
+package com.scmloader.test3;
+
+public class Main {
+
+    public static void mian(String args[]) {
+        Main m = new Main();
+        m.test();
+    }
+
+    public void test() {
+        System.out.println("Test1");
+    }
+}
