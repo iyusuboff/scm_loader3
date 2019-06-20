5d24f86606a2c3cf18ac98455ae8f5576313ce03
i.yusubov555@gmail.com <i.yusubov555@gmail.com>
2019-06-20 12:18:18 +0400

    Test2 added

--- com.scmloader.test3/Main.java
+++ com.scmloader.test3/Main.java
@@ -5,9 +5,14 @@ public class Main {
     public static void mian(String args[]) {
         Main m = new Main();
         m.test();
+        m.test2();
     }
 
     public void test() {
         System.out.println("Test1");
+    }
+
+    public void test2() {
+        System.out.println("Test2");
     }
 }
