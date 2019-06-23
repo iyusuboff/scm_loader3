4ef079608d2f0f35858b8f64389f709b7ece61ca
i.yusubov555@gmail.com <i.yusubov555@gmail.com>
2019-06-24 01:53:01 +0400

    Sample file added

---
+++ com.scmloader.test3/Sample.java
@@ -0,0 +1,44 @@
+package com.scmloader.test3;
+
+public class Test{
+
+    public static void mian(String args[]) {
+        Main m = new Main();
+        m.test();
+        m.test2();
+        m.test3();
+        m.test4();
+    }
+
+    public void test() {
+        System.out.println("Test1");
+    }
+
+    public void test2() {
+        System.out.println("Test2");
+    }
+
+    public void test3() {
+        System.out.println("Test3");
+    }
+
+    public void test4() {
+        System.out.println("Test3");
+    }
+
+    public void test() {
+        System.out.println("Test1");
+    }
+
+    public void test2() {
+        System.out.println("Test2");
+    }
+
+    public void test3() {
+        System.out.println("Test3");
+    }
+
+    public void test4() {
+        System.out.println("Test3");
+    }
+}
